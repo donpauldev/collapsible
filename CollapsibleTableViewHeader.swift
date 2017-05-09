4d225d5a9e27dafa0c7f1a42c2f7cfc295982434
Don Paul <donpaulpm92@gmail.com>
2017-05-09 18:32:18 +0530

    Create CollapsibleTableViewHeader.swift

---
+++ CollapsibleTableViewHeader.swift
@@ -0,0 +1,125 @@
+//
+//  CollapsibleTableViewHeader.swift
+//  dalleel
+//
+//  Created by AJESHKUMAR CHEMPANKULAM DIVAKARAN on 10/03/17.
+//  Copyright © 2017 a2solutions. All rights reserved.
+//
+
+import Foundation
+protocol CollapsibleTableViewHeaderDelegate {
+    func toggleSection(_ header: CollapsibleTableViewHeader, section: Int)
+}
+
+class CollapsibleTableViewHeader: UITableViewHeaderFooterView {
+    
+    var delegate: CollapsibleTableViewHeaderDelegate?
+    var section: Int = 0
+    
+    let titleLabel = UILabel()
+    let arrowLabel = UILabel()
+    
+    override init(reuseIdentifier: String?) {
+        super.init(reuseIdentifier: reuseIdentifier)
+        
+        
+        //
+        // Constraint the size of arrow label for auto layout
+        //
+        arrowLabel.widthAnchor.constraint(equalToConstant: 12).isActive = true
+        arrowLabel.heightAnchor.constraint(equalToConstant: 12).isActive = true
+        
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        arrowLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        contentView.addSubview(titleLabel)
+        contentView.addSubview(arrowLabel)
+        
+        
+        
+        
+        //
+        // Call tapHeader when tapping on this header
+        //
+        addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(CollapsibleTableViewHeader.tapHeader(_:))))
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        contentView.backgroundColor = UIColor(hex: 0xFFFFFF)
+        
+        contentView.layer.cornerRadius = 2
+        
+//        contentView.layer.shadowOpacity = 1
+//        contentView.layer.shadowOffset = CGSize.zero
+//        contentView.layer.shadowRadius = 3
+//        contentView.layer.shadowColor = Utilities.GreyColourD().cgColor
+//
+//        
+//        contentView.layer.masksToBounds = true
+//        contentView.layer.borderColor = Utilities.GreyColourD().cgColor
+//        contentView.layer.borderWidth = 1
+        
+        titleLabel.textColor = UIColor.white
+        arrowLabel.textColor = UIColor.white
+        
+        //
+        // Autolayout the lables
+        //
+        let views = [
+            "titleLabel" : titleLabel,
+            "arrowLabel" : arrowLabel,
+            ]
+//        
+//        let views2 = [
+//            "views" : views
+//        ]
+//        views2
+        
+        
+        contentView.addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat: "H:|-40-[titleLabel]-[arrowLabel]-20-|",
+            options: [],
+            metrics: nil,
+            views: views
+        ))
+        
+        contentView.addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat: "V:|-[titleLabel]-|",
+            options: [],
+            metrics: nil,
+            views: views
+        ))
+        
+        contentView.addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat: "V:|-[arrowLabel]-|",
+            options: [],
+            metrics: nil,
+            views: views
+        ))
+    }
+    
+    //
+    // Trigger toggle section when tapping on the header
+    //
+    func tapHeader(_ gestureRecognizer: UITapGestureRecognizer) {
+        guard let cell = gestureRecognizer.view as? CollapsibleTableViewHeader else {
+            return
+        }
+        
+        delegate?.toggleSection(self, section: cell.section)
+    }
+    
+    func setCollapsed(_ collapsed: Bool) {
+        //
+        // Animate the arrow rotation (see Extensions.swf)
+        //
+        arrowLabel.rotate(collapsed ? 0.0 : CGFloat(M_PI_2))
+    }
+    
+}
